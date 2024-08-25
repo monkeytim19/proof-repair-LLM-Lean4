f73b4ab22332ebee4ea2929a8133ba75be2440a3
monkeytim19 <timwong1919@gmail.com>
2024-08-25 16:03:22 +0100

    changed naming issue

--- pipeline/utils/condor/verifier/condor_file.cmd
+++ pipeline/utils/condor/verifier/condor_file.cmd
@@ -3,10 +3,10 @@ universe        = vanilla
 # This defines the path of the executable we want to run.
 executable      = /vol/bitbucket/tcwong/individual_project/proof-repair-LLM-Lean4/pipeline/utils/condor/verifier/condor_instructions.sh 
 arguments       = $(Datapath) $(Indexpath) $(Job_num)
-output          = /vol/bitbucket/tcwong/individual_project/proof-repair-LLM-Lean4/pipeline/utils/condor/logging/output_files/${Job_num}.out
-error           = /vol/bitbucket/tcwong/individual_project/proof-repair-LLM-Lean4/pipeline/utils/condor/logging/error_files/${Job_num}.err
+output          = /vol/bitbucket/tcwong/individual_project/proof-repair-LLM-Lean4/pipeline/utils/condor/logging/output_files/$(Job_num).out
+error           = /vol/bitbucket/tcwong/individual_project/proof-repair-LLM-Lean4/pipeline/utils/condor/logging/error_files/$(Job_num).err
 
-log             = /vol/bitbucket/tcwong/individual_project/proof-repair-LLM-Lean4/pipeline/utils/condor/logging/process_files/${Job_num}.log
+log             = /vol/bitbucket/tcwong/individual_project/proof-repair-LLM-Lean4/pipeline/utils/condor/logging/process_files/$(Job_num).log
 
 request_cpus            = 8
 request_memory          = 8G
