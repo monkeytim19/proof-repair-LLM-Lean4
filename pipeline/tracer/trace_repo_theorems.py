b7b95b4303a3250b0d09b81353fcc17e85a2223c
Timothy <timwong1919@gmail.com>
2024-08-25 19:55:56 +0100

    removed comment and changed import

--- pipeline/tracer/trace_repo_theorems.py
+++ pipeline/tracer/trace_repo_theorems.py
@@ -47,51 +47,7 @@ def trace_directory(dir_path, repo_copy_path):
             thm_names_dict = {}
             thm_names_dict["name"], thm_names_dict["start"], thm_names_dict["end"] = name_info.split("&")
             thm_names.append(thm_names_dict)
-        # with open(blob_full_path, 'r') as file:
-        #     original_file = file.read()
-
-        # for idx, name in enumerate(thm_names):
-
-        #     thm_names_dict[name]["idx"] = idx
-
-        #     # retrieve the theorem statement and proof from printing directly in the Lean file
-        #     with open(blob_full_path, 'a') as file:
-        #         text_to_append = pretty_printer_options + [f"#print {name}"]
-        #         file.write("\n" + "\n".join(text_to_append))
-        #     thm_info, status = print_theorem_expr(name, module_path, repo_copy_path)
-    
-        #     if len(thm_info) > 0:
-                
-        #         # attempt to compile the extractd theorem and proof
-        #         thm_body_start_idx = thm_info.find(":")
-        #         temp_thm_start = "theorem temp "
-        #         temp_thm_info = temp_thm_start + thm_info[thm_body_start_idx:]
-        #         with open(blob_full_path, 'w') as file:
-        #             file.write(original_file + "\n" + temp_thm_info)
-        #         try:
-        #             subprocess.run(["lake", "build", module_path],
-        #                                 cwd=repo_copy_path,
-        #                                 capture_output=True,
-        #                                 text=True,
-        #                                 check=True
-        #                             )
-        #         except subprocess.CalledProcessError as err:
-        #             thm_names_dict[name]["status"] = "failed_compile"
-        #             print(f"FAILED TO COMPILE: {name} from {module_path}")
-        #         else:
-        #             # save if the compilation succeeded
-        #             print(f"SUCCESS: {name} from {module_path}")
-        #             thm_names_dict[name]["status"] = "success"
-        #             thm_statement, thm_proof = re.split(r"\n(?!\s)", thm_info)
-        #             thm_info = {"name": name, "statement": thm_statement, "proof": thm_proof}
-        #             save_data_to_json_in_dir(thm_info, f"{idx}.txt", module_data_dir_path)
-        #     else:
-        #         thm_names_dict[name]["status"] = status
-
-        #     # restore the file to original state
-        #     with open(blob_full_path, 'w') as file:
-        #         file.write(original_file)
-
+            
         # save the extracted theorem names to .json file
         save_data_to_json_in_dir(thm_names, module_path.replace(".", "_"), TRACED_INFO_DIR)
 
